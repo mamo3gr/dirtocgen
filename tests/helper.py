dc796815ce6ffdf67b0a7a146ed1ab1ef1c01b2d
Mamoru Miura <mamo3gr@gmail.com>
2023-05-13 23:53:05 +0900

    test: enable creates files and directories from string

--- tests/helper.py
+++ tests/helper.py
@@ -1,4 +1,6 @@
+import re
 from pathlib import Path
+from typing import IO
 
 
 def directory2string(root_dir: str | Path):
@@ -19,3 +21,61 @@ def directory2string(root_dir: str | Path):
                 ret_str += f.read()
 
     return ret_str
+
+
+class BufferedFileWriter:
+    def __init__(self):
+        self.file: None | IO = None
+        self.string: None | str = None
+
+    def open(self, filename):
+        if self.file:
+            self.file.close()
+
+        self.file = open(filename, "w")
+
+    def append_line(self, string):
+        if self.string is None:
+            self.string = string
+        else:
+            self.string += f"\n{string}"
+
+    def flush_and_close(self):
+        if self.file is None:
+            return
+
+        self.file.write(self.string)
+        self.string = None
+
+        self.file.close()
+        self.file = None
+
+
+def string2directory(root_dir: str | Path, string: str):
+    """
+    Helper function, which creates files and directories
+    with respect to given string.
+    """
+    root_dir = Path(root_dir)
+    writer = BufferedFileWriter()
+
+    for line in string.split("\n"):
+        dir_operation = re.match(r"^\[DIRECTORY] (.*)$", line)
+        if dir_operation:
+            writer.flush_and_close()
+
+            directory = root_dir / dir_operation.group(1)
+            directory.mkdir()
+            continue
+
+        file_operation = re.match(r"^\[FILE] (.*)$", line)
+        if file_operation:
+            writer.flush_and_close()
+
+            filename = root_dir / file_operation.group(1)
+            writer.open(filename)
+            continue
+
+        writer.append_line(line)
+
+    writer.flush_and_close()
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -4,7 +4,7 @@ from tempfile import TemporaryDirectory
 from textwrap import dedent
 from unittest import TestCase
 
-from tests.helper import directory2string
+from tests.helper import directory2string, string2directory
 
 
 class TestDirectory2string(TestCase):
@@ -70,3 +70,103 @@ class TestDirectory2string(TestCase):
                 """
             )
             self.assertEqual(expect, directory2string(tmpd))
+
+
+class TestString2directory(TestCase):
+    def test_directory(self):
+        with TemporaryDirectory() as tmpd:
+            string = dedent(
+                """\
+                [DIRECTORY] my_dir
+                """
+            ).strip("\n")
+            string2directory(tmpd, string)
+
+            _tmpd = Path(tmpd)
+            self.assertTrue((_tmpd / "my_dir").is_dir())
+
+    def test_emptyfile(self):
+        with TemporaryDirectory() as tmpd:
+            string = dedent(
+                """\
+                [FILE] doc.md
+                """
+            )
+            string2directory(tmpd, string)
+
+            with open(os.path.join(tmpd, "doc.md")) as f:
+                content_actual = f.read()
+                content_expect = ""
+                self.assertEqual(content_expect, content_actual)
+
+    def test_file(self):
+        with TemporaryDirectory() as tmpd:
+            string = dedent(
+                """\
+                [FILE] doc.md
+                # Title
+
+                body"""
+            )
+            string2directory(tmpd, string)
+
+            with open(os.path.join(tmpd, "doc.md")) as f:
+                content_actual = f.read()
+                content_expect = dedent(
+                    """\
+                    # Title
+
+                    body"""
+                )
+                self.assertEqual(content_expect, content_actual)
+
+    def test_multiple_files(self):
+        with TemporaryDirectory() as tmpd:
+            string = dedent(
+                """\
+                [FILE] doc1.md
+                # Title1
+
+                body1
+                [FILE] doc2.md
+                # Title2
+
+                body2"""
+            )
+            string2directory(tmpd, string)
+
+            with open(os.path.join(tmpd, "doc1.md")) as f:
+                content_actual = f.read()
+                content_expect = dedent(
+                    """\
+                    # Title1
+
+                    body1"""
+                )
+                self.assertEqual(content_expect, content_actual)
+
+            with open(os.path.join(tmpd, "doc2.md")) as f:
+                content_actual = f.read()
+                content_expect = dedent(
+                    """\
+                    # Title2
+
+                    body2"""
+                )
+                self.assertEqual(content_expect, content_actual)
+
+    def test_nested_file(self):
+        with TemporaryDirectory() as tmpd:
+            string = dedent(
+                """\
+                [DIRECTORY] foo
+                [DIRECTORY] foo/bar
+                [FILE] foo/bar/baz.md
+                """
+            )
+            string2directory(tmpd, string)
+
+            _tmpd = Path(tmpd)
+            self.assertTrue((_tmpd / "foo").is_dir())
+            self.assertTrue((_tmpd / "foo" / "bar").is_dir())
+            self.assertTrue((_tmpd / "foo" / "bar" / "baz.md").exists())
